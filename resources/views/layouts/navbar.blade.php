51c3808785f71bf1ac68bcee1aa891555c29b834
Dylan Cruse <dylancruse@live.com>
2021-04-20 22:10:50 -0500

    Add color variables, video game font, font awesome icons, update navbar and main html structure

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,13 +1,18 @@
 
-<div id="navbar" class="navbar full-width white purple-background">
-    <div class="flex justify-content-end full-width px-6">
+<div id="navbar" class="navbar full-width white sky-blue-background">
+    <div class="flex half-width px-6">
+        <i class="fas fa-cat fa-2x"></i>
+        {{-- <i class="fab fa-angellist fa-2x"></i> --}}
+        
+    </div>
+    <div class="flex justify-content-end half-width px-6">
         @auth
-            <a href="{{ url('/home') }}" class="text-md underline">Home</a>
+            <a href="{{ url('/home') }}" class="text-md underline white">Home</a>
         @else
-            <a href="{{ route('login') }}" class="text-md underline">Log in</a>
+            <a href="{{ route('login') }}" class="text-md underline white">Log in</a>
     
             @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-md underline">Register</a>
+                <a href="{{ route('register') }}" class="ml-4 text-md underline white">Register</a>
             @endif
         @endauth
     </div>
