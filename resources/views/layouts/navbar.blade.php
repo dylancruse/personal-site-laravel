799b27c6fd63e9f73a0dbee473e8d9f0d210f8ca
Dylan Cruse <dylancruse@live.com>
2021-04-23 22:22:39 -0500

    Update typography and color, update navbar and card styles

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
 
-<div id="navbar" class="navbar full-width white sky-blue-background">
+<div id="navbar" class="navbar">
     <div class="flex half-width px-6">
         <a href="{{ url('/home') }}">
             <i class="fas fa-cat fa-2x"></i>
@@ -9,12 +9,12 @@
     </div>
     <div class="flex justify-content-end half-width px-6">
         @auth
-            <a href="{{ url('/home') }}" class="text-md underline white">Home</a>
+            <a href="{{ url('/home') }}" class="text-md white">Home</a>
         @else
-            <a href="{{ route('login') }}" class="text-md underline white">Log in</a>
+            <a href="{{ route('login') }}" class="text-md white">Log in</a>
     
             @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-md underline white">Register</a>
+                <a href="{{ route('register') }}" class="ml-4 text-md white">Register</a>
             @endif
         @endauth
     </div>
