2af9e0d263207c591a6c34c2a352b7cc3b24c462
Dylan Cruse <dylancruse@live.com>
2021-05-02 14:56:51 -0500

    New login page, lots of style/layout changes, rearrange sass directory

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 
 <div id="navbar" class="navbar">
     <div class="flex half-width px-6">
-        <a href="{{ url('/home') }}">
+        <a href="{{ url('/home') }}" class="nav-link">
             <i class="fas fa-cat fa-2x"></i>
         </a>
         {{-- <i class="fab fa-angellist fa-2x"></i> --}}
@@ -9,13 +9,12 @@
     </div>
     <div class="flex justify-content-end half-width px-6">
         @auth
-            <a href="{{ url('/home') }}" class="text-md white nav-link">Home</a>
             <form action="{{ route('logout') }}" method="post">
                 @csrf
                 <button type="submit" class="nav-button text-md ml-4">Logout</button>
             </form>
         @else
-            <a href="{{ route('login') }}" class="nav-link text-md white">Log in</a>
+            <a href="{{ route('login') }}" class="nav-link text-md white">Login</a>
             <a href="{{ route('register') }}" class="nav-link text-md white ml-4">Register</a>
         @endauth
     </div>
