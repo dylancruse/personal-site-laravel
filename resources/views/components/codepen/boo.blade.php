097118aca69e7866c20173d71f1a470a8a6cc2ce
Dylan Cruse <dylancruse@live.com>
2021-05-04 14:23:00 -0500

    Update homepage codepen section

---
+++ resources/views/components/codepen/boo.blade.php
@@ -0,0 +1,6 @@
+<p class="codepen" data-height="350" data-theme-id="dark" data-default-tab="result" data-user="dylancruse" data-slug-hash="yLLomOj" data-preview="true" style="height: 350px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="Halloween with Stylus">
+  <span>See the Pen <a href="https://codepen.io/dylancruse/pen/yLLomOj">
+  Halloween with Stylus</a> by Dylan Cruse (<a href="https://codepen.io/dylancruse">@dylancruse</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
